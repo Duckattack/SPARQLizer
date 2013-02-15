d2b40889f2e94b498d753000abca6954ddca346c
Carina Haupt <carina.haupt@scai-extern.fraunhofer.de>
2013-02-15 16:55:06 +0100

    Added automcompletion support for graphs. For predicates is prepared but not yet working.

--- src/de/caha42/sparqlizer/QueryActivity.java
+++ src/de/caha42/sparqlizer/QueryActivity.java
@@ -1,40 +1,82 @@
 package de.caha42.sparqlizer;
 
+import com.hp.hpl.jena.query.ResultSet;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.Toast;
 
-import com.hp.hpl.jena.query.ResultSet;
-
 import de.caha42.sparqlizer.task.SPARQLExecutionTask;
+import de.caha42.sparqlizer.utils.SPARQLUtils;
 
 public class QueryActivity extends Activity {
 
 	public final static String SPARQL_RESULT = "de.caha42.sparqlizer.SPARQLRESULT";
 	
 	private EditText endpointUri;
-	private EditText sparqlQuery;
-	private View sendSparqlQuery;
+	private AutoCompleteTextView defaultGraph;
+	private MultiAutoCompleteTextView sparqlQuery;
+	
+	private Button sendGraphQuery;
+	private Button sendPredicateQuery;
+	private Button sendSparqlQuery;
 
+	private String[] graphs = new String[0];
+	private String[] predicates = new String[0];
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_query);
 
+		// Textfields
 		endpointUri = (EditText) findViewById(R.id.endpoint);
-		endpointUri.setText("http://ops-virtuoso.scai.fraunhofer.de:8893/sparql");
-		sparqlQuery = (EditText) findViewById(R.id.sparql_query);
+		endpointUri.setText("http://ops-virtuoso.scai.fraunhofer.de:8892/sparql");
+		
+		defaultGraph = (AutoCompleteTextView) findViewById(R.id.graph);
+		
+		sparqlQuery = (MultiAutoCompleteTextView) findViewById(R.id.sparql_query);
 		sparqlQuery.setText("SELECT * WHERE {?s ?p ?o .} LIMIT 5");
+
+		// Buttons		
+		sendGraphQuery = (Button) findViewById(R.id.graph_query_send);
+		sendGraphQuery.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				ResultSet resultSet = runSparqlQuery(endpointUri.getText().toString(), "SELECT DISTINCT ?g WHERE { GRAPH ?g { ?s ?p ?o } }");
+				graphs = SPARQLUtils.formatResultSetAsStringArray(resultSet);
+				defaultGraph.setAdapter(new ArrayAdapter<String>(v.getContext(),
+		                android.R.layout.simple_dropdown_item_1line, graphs));
+			}
+		});
+		
+		sendPredicateQuery = (Button) findViewById(R.id.predicate_query_send);
+		sendPredicateQuery.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				ResultSet resultSet = runSparqlQuery(endpointUri.getText().toString(), "SELECT DISTINCT ?p WHERE {?s ?p ?o}");
+				predicates = SPARQLUtils.formatResultSetAsStringArray(resultSet);
+				sparqlQuery.setAdapter(new ArrayAdapter<String>(v.getContext(),
+		                 android.R.layout.simple_dropdown_item_1line, predicates));
+				sparqlQuery.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+			}
+		});
+		
 
 		sendSparqlQuery = (Button) findViewById(R.id.sparql_query_send);
 		sendSparqlQuery.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				runSparqlQuery(endpointUri.getText().toString(), sparqlQuery.getText().toString());
+				ResultSet resultSet = runSparqlQuery(endpointUri.getText().toString(), defaultGraph.getText().toString(), sparqlQuery.getText().toString());
+				String response = SPARQLUtils.formatResultSetAsString(resultSet);
+				Intent intent = new Intent(v.getContext(), ResultActivity.class);
+				intent.putExtra(SPARQL_RESULT, response);
+				startActivity(intent);
 			}
 		});
 	}
@@ -53,19 +95,30 @@ public class QueryActivity extends Activity {
 	 * @return
 	 * @throws Exception 
 	 */
-	private void runSparqlQuery(String endpoint, String queryString) {
+	private ResultSet runSparqlQuery(String endpoint, String queryString) {
+		return runSparqlQuery(endpoint, "", queryString);
+	}
+	
+	/**
+	 * Execute SPARQL query
+	 * @param endpoint 
+	 * @param defaultGraph 
+	 * @param queryString
+	 * @return
+	 * @throws Exception 
+	 */
+	private ResultSet runSparqlQuery(String endpoint, String defaultGraph, String queryString) {
 		SPARQLExecutionTask sparqlExecutionTask = new SPARQLExecutionTask(this);
-		sparqlExecutionTask.execute(endpoint, queryString);
+		sparqlExecutionTask.execute(endpoint, defaultGraph, queryString);
 
-		String response = null;
+		ResultSet response = null;
 		try {
 			response = sparqlExecutionTask.get();
 		} catch (Exception e) {
 			Toast.makeText(this, "An error occured: " + e.getMessage(), Toast.LENGTH_LONG).show();
 		}
 		
-		Intent intent = new Intent(this, ResultActivity.class);
-		intent.putExtra(SPARQL_RESULT, response);
-		startActivity(intent);
+		return response;
 	}
+	
 }
--- src/de/caha42/sparqlizer/task/SPARQLExecutionTask.java
+++ src/de/caha42/sparqlizer/task/SPARQLExecutionTask.java
@@ -1,7 +1,5 @@
 package de.caha42.sparqlizer.task;
 
-import java.util.List;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
@@ -10,11 +8,10 @@ import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
-import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.Syntax;
 
-public class SPARQLExecutionTask extends AsyncTask<String, Integer, String> {
+public class SPARQLExecutionTask extends AsyncTask<String, Integer, ResultSet> {
 
 	private Activity context;
 	private ProgressDialog dialog;
@@ -31,62 +28,42 @@ public class SPARQLExecutionTask extends AsyncTask<String, Integer, String> {
 	} 
 
 	@Override
-	protected String doInBackground(String... params) {
+	protected ResultSet doInBackground(String... params) {
 		String sparqlEndpointUri = params[0];
-		String queryString = params[1];
+		String defaultGraph = params[1];
+		String queryString = params[2];
 
 		// Create a Query instance
 		Query query = QueryFactory.create(queryString, Syntax.syntaxARQ);
 
 		// Limit the number of results returned
 		// Setting the limit is optional - default is unlimited
-		query.setLimit(10);
+		//		query.setLimit(10);
 
 		// Set the starting record for results returned
 		// Setting the limit is optional - default is 1 (and it is 1-based)
-		query.setOffset(11);
+		//		query.setOffset(11);
 
 		// This query uses an external SPARQL endpoint for processing
 		// This is the syntax for that type of query
-		QueryExecution qe = QueryExecutionFactory.sparqlService(sparqlEndpointUri, query);
-
+		QueryExecution qe;
+		if (defaultGraph == null || defaultGraph.trim().length() == 0) {
+			qe = QueryExecutionFactory.sparqlService(sparqlEndpointUri, query);
+		} else {
+			qe = QueryExecutionFactory.sparqlService(sparqlEndpointUri, query, defaultGraph);
+		}
 		// Execute the query and obtain results
 		ResultSet resultSet = qe.execSelect();
 
-		// Get the column names (the aliases supplied in the SELECT clause)
-		List<String> columnNames = resultSet.getResultVars();
-
-		// Setup a place to house results for output
-		StringBuffer response = new StringBuffer();
-
-		// Iterate through all resulting rows
-		while (resultSet.hasNext()) {
-			// Get the next result row
-			QuerySolution solution = resultSet.next();
-
-			for (String columnName : columnNames) {
-				// Data value will be null if optional and not present
-				if (solution.get(columnName) == null) {
-					response.append("{null}");
-					// Test whether the returned value is a literal value
-				} else if (solution.get(columnName).isLiteral()) {
-					response.append(solution.getLiteral(columnName).toString());
-					// Otherwise the returned value is a URI
-				} else {
-					response.append(solution.getResource(columnName).getURI());
-				}
-				response.append('\n');
-			}
-			response.append("-----------------\n");
-		}
 		// Important - free up resources used running the query
 		qe.close();
 
-		return response.toString();
+		return resultSet;
 	}
 
+
 	@Override
-	protected void onPostExecute(String response) {
+	protected void onPostExecute(ResultSet resultSet) {
 		if (dialog.isShowing()) {
 			dialog.dismiss();
 		}
